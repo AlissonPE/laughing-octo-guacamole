9e249fbd8590d8d357aab5ca3522fc395a073b2f
alisson <alisson.almeida342@gmail.com>
2022-02-07 17:51:11 -0300

    desafio proposto por mentor, fibonacci

---
+++ desafio.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+
+public class desafio {
+
+    public static void main(String[] args) {
+        int ultimo = 0;
+        int penultimo = 1;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos números da sequencia de fibonacci compararemos e somaremos os números pares? :  ");
+        int fibo = sc.nextInt();
+
+        int contador = 0;
+        int soma = 0;
+
+        while (contador < fibo) {
+            //* Condição para que o While termine.
+            contador = contador + 1;
+
+            //System.out.println("Entrando no metodo");
+            int resultado = ultimo + penultimo;  // ou penultimo + ultimo = resultado
+            //  resultado <- ultimo <- penultimo
+            penultimo = ultimo;
+            ultimo = resultado;
+
+
+            System.out.println("Sequencia: " + resultado);
+
+            if (resultado % 2 == 0) {
+                soma = soma + resultado;
+
+                System.out.println("achado o número par: " + resultado + " : " + soma);
+
+            }
+            if (resultado > 4000000) {
+                System.out.println("EPA, ENCONTRAMOS O NÚMERO PREMIADO: " + contador);
+                break;
+            }
+            sc.close();
+        }
+    }
+}
+
